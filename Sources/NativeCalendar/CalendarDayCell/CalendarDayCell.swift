f56b5c0fb58f29d24b98f827d145073b7bfc3ff5
Ahmed Talaat <ahmedtal@banquemisr.com>
2024-02-04 16:36:00 +0200

    Update CalendarDayCell.swift

--- Sources/NativeCalendar/CalendarDayCell/CalendarDayCell.swift
+++ Sources/NativeCalendar/CalendarDayCell/CalendarDayCell.swift
@@ -24,7 +24,7 @@ public class CalendarDayCell: UICollectionViewCell {
         super.awakeFromNib()
         isAccessibilityElement = true
         accessibilityTraits = .button
-//        setupView()
+        setupView()
     }
     
     func setDay(day: Day?, isMonthView: Bool){
