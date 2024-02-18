c028362e66e8c2d92c7f9fe359a7084525a17bba
Ahmed Talaat <ahmedtal@banquemisr.com>
2024-02-18 16:34:33 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NativeCalendar",
-            dependencies: [],
+            dependencies: ["TestPackage"],
             path: "Sources/NativeCalendar",
             resources: [.process("SupportingFiles")]
         ),
