81d8bc32cc7cc9fb1d7d89461f46dada6c285505
Ahmed Talaat <ahmedtal@banquemisr.com>
2024-02-18 16:41:36 +0200

    remove package

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
             targets: ["NativeCalendar"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ahmedtalaatshaker/TestPackage", from: "1.0.1")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -21,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NativeCalendar",
-            dependencies: ["TestPackage"],
+            dependencies: [],
             path: "Sources/NativeCalendar",
             resources: [.process("SupportingFiles")]
         ),
