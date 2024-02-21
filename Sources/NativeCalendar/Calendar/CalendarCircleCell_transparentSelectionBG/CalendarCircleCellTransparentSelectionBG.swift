8519d6d4a91a1b04ac1a676dd6a019dd52bfa7a3
Ahmed Talaat <ahmedtal@banquemisr.com>
2024-02-21 09:58:27 +0200

    Update CalendarCircleCellTransparentSelectionBG.swift

--- Sources/NativeCalendar/Calendar/CalendarCircleCell_transparentSelectionBG/CalendarCircleCellTransparentSelectionBG.swift
+++ Sources/NativeCalendar/Calendar/CalendarCircleCell_transparentSelectionBG/CalendarCircleCellTransparentSelectionBG.swift
@@ -22,16 +22,30 @@ class CalendarCircleCellTransparentSelectionBG: CalendarDayCell {
         selectionBackgroundView.setGradientBackground(colors: selectedBGColor)
         resetView()
         
-        guard let color1 = selectedBGColor.first?.components, let color2 = selectedBGColor[1].components else { return }
-        let newColor = CGColor(red: color1[0], green: color2[1], blue: color1[2], alpha: 0.3)
-        leftBG.backgroundColor = UIColor(cgColor: newColor) //UIColor(cgColor: color).withAlphaComponent(0.5)
-        rightBG.backgroundColor = UIColor(cgColor: newColor) //UIColor(cgColor: color).withAlphaComponent(0.5)
+//        guard let color1 = selectedBGColor.first?.components, let color2 = selectedBGColor[1].components else { return }
+//        let newColor = CGColor(red: color1[0], green: color2[1], blue: color1[2], alpha: 0.3)
+        leftBG.backgroundColor = colorWithGradient(frame: leftBG.frame, colors: selectedBGColor).withAlphaComponent(0.3) //UIColor(cgColor: color).withAlphaComponent(0.5)
+        rightBG.backgroundColor = colorWithGradient(frame: rightBG.frame, colors: selectedBGColor).withAlphaComponent(0.3) //UIColor(cgColor: color).withAlphaComponent(0.5)
 
 //        guard let color = selectedBGColor.first else { return }
 //        leftBG.backgroundColor = UIColor(cgColor: color).withAlphaComponent(0.5)
 //        rightBG.backgroundColor = UIColor(cgColor: color).withAlphaComponent(0.5)
     }
 
+    func colorWithGradient(frame: CGRect, colors: [CGColor]) -> UIColor {
+        // create the background layer that will hold the gradient
+        let backgroundGradientLayer = CAGradientLayer()
+        backgroundGradientLayer.frame = frame
+        // we create an array of CG colors from out UIColor array
+//        let cgColors = colors.map({$0})
+        backgroundGradientLayer.colors = colors
+        UIGraphicsBeginImageContext(backgroundGradientLayer.bounds.size)
+        backgroundGradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+        let backgroundColorImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return UIColor(patternImage: backgroundColorImage!)
+    }
+    
     override func setFromTo(text: String) {
         fromToLabel.isHidden = false
         fromToLabel.text = text
